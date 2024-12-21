38fa4127ce938e5a5b0565ca97dd5cf837b189cf
senyagame <arseniyantropov3@gmail.com>
2024-12-21 20:30:22 +0500

    commit for TG

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(API_KEY)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
-    web_app = types.WebAppInfo("https://your-web-app-url.com")  # Замени на URL твоего веб-приложения
+    web_app = types.WebAppInfo("https://supper-app-two.vercel.app/weather-app.html")  # Замени на URL твоего веб-приложения
     button = types.InlineKeyboardButton("Открыть приложение", web_app=web_app)
     markup.add(button)
     bot.send_message(message.chat.id, "Привет! Нажми на кнопку ниже, чтобы открыть приложение:", reply_markup=markup)
